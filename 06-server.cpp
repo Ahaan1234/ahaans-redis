f668fecca975b2c4860688bba4765965a5a42a48
Ahaan1234 <1ahaanshah@gmail.com>
2026-06-11 18:15:15 +0530

    got it working!!

--- 06-server.cpp
+++ 06-server.cpp
@@ -33,73 +33,11 @@ static void die(const char *msg) {
     exit(1);
 }
 
-/*static void msg(const char *msg) {
+static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
 
-static int32_t read_full(int fd, char *buf, size_t n) {
-    while (n > 0) {
-        ssize_t rv = read(fd, buf, n); 
-        if (rv <= 0) {
-            return -1;
-        }
-        assert((size_t)rv <= n);
-        n -= (size_t)rv; // size_t—the standard unsigned integer type to represent sizes and array indices in bytes
-        buf += rv;
-    }
-    return 0;
-}
-
-static int32_t write_all(int fd, const char *buf, size_t n) {
-    while (n > 0) {
-        ssize_t rv = write(fd, buf, n);
-        if (rv <= 0) {
-            return -1;  // error
-        }
-        assert((size_t)rv <= n);
-        n -= (size_t)rv;
-        buf += rv;
-    }
-    return 0;
-}*/
-
 const size_t k_max_msg = 4096;
-/*
-static int32_t one_request(int connfd) {
-    // 4 bytes header
-    char rbuf[4 + k_max_msg];
-    errno = 0;
-    int32_t err = read_full(connfd, rbuf, 4);
-    if (err) {
-        msg(errno == 0 ? "EOF" : "read() error"); // (condition ? value_if_true : value_if_false)
-        return err;
-    }
-
-    uint32_t len = 0;
-    memcpy(&len, rbuf, 4); // short for memory copy; function used to rapidly copy a specific number of bytes from a source memory location to a destination memory location
-    if (len > k_max_msg) {
-        msg("too long");
-        return -1;
-    }
-
-    // request body
-    err = read_full(connfd, &rbuf[4], len);
-    if (err) {
-        msg("read() error");
-        return err;
-    }
-
-    // do stuff
-    printf("client says: %.*s\n", len, &rbuf[4]);
-
-    // reply using same protocol
-    const char reply[] = "world";
-    char wbuf[4 + sizeof(reply)];
-    len = (uint32_t)strlen(reply);
-    memcpy(wbuf, &len, 4);
-    memcpy(&wbuf[4], reply, len);
-    return write_all(connfd, wbuf, len + 4);
-}*/
 
 /*
     * With an event loop, an application task can span multiple loop iterations, 
@@ -179,7 +117,7 @@ static void handle_read(Conn *conn) {
         return;
     }
     buf_append(conn->incoming, buf, (size_t)rv);
-    try_one_request(conn);
+    while (try_one_request(conn)) {}
     if(conn->outgoing.size() > 0) {
         conn->want_read = false;
         conn->want_write = true;
@@ -254,10 +192,10 @@ int main() {
         for(size_t i = 1; i < poll_args.size(); ++i) {
             uint32_t ready = poll_args[i].revents;
             Conn *conn = fd2conn[poll_args[i].fd];
-            if(ready && POLLIN) {
+            if(ready & POLLIN) {
                 handle_read(conn);
             }
-            if(ready && POLLOUT) {
+            if(ready & POLLOUT) {
                 handle_write(conn);
             }
             if ((ready & POLLERR) || conn->want_close) {
